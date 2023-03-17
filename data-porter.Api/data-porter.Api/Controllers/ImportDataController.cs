9d7e2b8f3d2b41b0d57ba8a8dea37efb3feabe28
Sarath Pantula <sarath.pantula@gmail.com>
2023-03-18 04:55:24 +0530

    First cut

---
+++ data-porter.Api/data-porter.Api/Controllers/ImportDataController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace data_porter.Api.Controllers;
+
+/// <summary>
+/// Import Data Controller, this controller is responsible for uploading and downloading files
+/// </summary>
+[Route("api/[controller]")]
+[ApiController]
+public class ImportDataController : ControllerBase
+{
+
+    private readonly CloudBlobContainer _blobContainer;
+    //private readonly string _connectionString = "your postgres connection string";
+    /// <summary>
+    /// 
+    /// </summary>
+    public ImportDataController()
+    {
+        // Initialize Azure Blob Storage
+        CloudStorageAccount storageAccount = CloudStorageAccount.Parse("");
+        CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+        _blobContainer = blobClient.GetContainerReference("excel");
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost]
+    [Route("upload")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> ImportData([FromForm] IFormFile file)
+    {
+        // Read the Excel file from the request
+        using Stream stream = file.OpenReadStream();
+
+        // Generate a unique ID for the file
+        Guid fileId = Guid.NewGuid();
+
+        // Upload the file to Azure Blob Storage
+        CloudBlockBlob blockBlob = _blobContainer.GetBlockBlobReference(fileId.ToString());
+        blockBlob.Properties.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+        await blockBlob.UploadFromStreamAsync(stream);
+
+        // Store the ID in Postgres database
+        //using var connection = new NpgsqlConnection(_connectionString);
+        //connection.Open();
+
+        //using var cmd = new NpgsqlCommand("INSERT INTO ExcelFiles (Id) VALUES (@id)", connection);
+        //cmd.Parameters.AddWithValue("id", fileId);
+        //await cmd.ExecuteNonQueryAsync();
+
+        return Ok(new { fileId });
+    }
+
+    /// <summary>
+    /// Download File
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpGet("files/{id}")]
+    public async Task<IActionResult> DownloadFile(string id)
+    {
+        // Get the file from Azure Blob Storage using its ID
+        CloudBlockBlob blob = _blobContainer.GetBlockBlobReference(id);
+
+        if (!await blob.ExistsAsync())
+        {
+            return NotFound();
+        }
+
+        // Set the response headers
+        Response.Headers.Add("Content-Disposition", $"attachment; filename={blob.Name}");
+        Response.Headers.Add("Content-Type", blob.Properties.ContentType);
+
+        // Download the file and return it as a stream
+        Stream stream = await blob.OpenReadAsync();
+        return File(stream, blob.Properties.ContentType, blob.Name);
+    }
+
+}
--- data-porter.Api/data-porter.Api/Program.cs
+++ data-porter.Api/data-porter.Api/Program.cs
@@ -1,22 +1,86 @@
+using core.Extensions.StartUpExtensions;
+using Serilog;
+using Serilog.Events;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Limits.MaxRequestBodySize = 50 * 1024 * 1024; // 50 MB
+});
+
+builder.Logging.ClearProviders();
+
+builder.Host.UseSerilog((hostname, services, configuration) =>
+{
+    configuration
+    .WriteTo.File("Logs/data-porter.log",
+    restrictedToMinimumLevel: LogEventLevel.Debug,
+    rollingInterval: RollingInterval.Day)
+    .WriteTo.Console();
+});
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+builder.Services.ConfigureBaseExtensions(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
 }
 
+app.UseSwagger(c =>
+{
+    c.SerializeAsV2 = true;
+});
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Data Porter v1");
+});
+
+app.UseSerilogRequestLogging();
+
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+    await next.Invoke();
+});
+
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseCors(builder => builder.WithOrigins("*")
+.AllowAnyHeader()
+.AllowAnyMethod()
+.AllowAnyOrigin());
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
